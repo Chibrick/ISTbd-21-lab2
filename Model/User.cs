d3cfc971fcdc57e123c67d953ffd364a2b2d6301
Chibrick <86246237+Chibrick@users.noreply.github.com>
2021-12-13 12:32:47 +0400

    Add files via upload

---
+++ Model/User.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseWork.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Comment("The nickname of user")]
+        public string Nickname { get; set; }
+
+
+        [Comment("The password of user")]
+        public string Password { get; set; }
+
+        public User() { }
+
+        public User(string login, string Pass)
+        {
+            this.Nickname = login;
+            this.Password = Pass;
+        }
+
+    }
+}
